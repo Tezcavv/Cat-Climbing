0033898ca2e20850ef633c6d5bce5d7638ad6900
Tezcavv <vittorio.vernassa@studio.unibo.it>
2023-01-26 21:52:03 +0100

    mi sa dovrò cambiare tutto
    perché sto  facendo questo

--- Assets/Script/TerrainManager.cs
+++ Assets/Script/TerrainManager.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class TerrainManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject obstacle;
+    [SerializeField]
+    private int numberOfOstacles;
+    private List<GameObject> obstacles;
+    private float randomNum;
     // Start is called before the first frame update
     void Start()
     {
-        
+        obstacles = new List<GameObject>();
+        for(int i = 0; i < numberOfOstacles; i++)
+        {
+            SpawnObstacles();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(GameObject ob in obstacles)
+        {
+            ob.transform.SetPositionAndRotation(transform.position + new Vector3(0, 1, 0), transform.rotation);
+        }
     }
 
-    public void SpawnObstacles() { }
+    public void SpawnObstacles() {
+
+        GameObject temp = GameObject.Instantiate(obstacle);
+        obstacles.Add(temp);
+
+    }
 }
