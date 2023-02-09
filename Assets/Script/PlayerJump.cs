b01348f4c80307a930ea9258399fbcf340fd161c
Tezcavv <vittorio.vernassa@studio.unibo.it>
2023-02-09 22:17:46 +0100

    cancellato il player

--- Assets/Script/PlayerJump.cs
+++ Assets/Script/PlayerJump.cs
@@ -9,11 +9,19 @@ public class PlayerJump : MonoBehaviour
     private float jumpForce;
     Rigidbody body;
     public bool canJump;
+    [SerializeField]
+    private float rayCastDistance;
+
+    RaycastHit hit;
+
+    private bool hasHit;
+    private Vector3 oldPos;
     // Start is called before the first frame update
     void Start()
     {
         body= GetComponent<Rigidbody>();
         canJump= true;
+        oldPos= transform.position;
     }
 
     // Update is called once per frame
@@ -24,14 +32,35 @@ public class PlayerJump : MonoBehaviour
         }
     }
 
+    private void FixedUpdate() {
+        ManageJumping();
+        RecordOldPos();
+    }
+
+    void RecordOldPos() {
+        oldPos= transform.position;
+    }
+
     void Jump() {
         canJump= false;
         body.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
     }
 
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Ground")) {
-            canJump= true;
+    bool IsFalling() {
+        return oldPos.y - transform.position.y > 0;
+    }
+
+    void ManageJumping() {
+
+        if (!IsFalling()) {
+            return;
         }
+
+        hasHit = Physics.Raycast(gameObject.transform.position, Vector3.down, rayCastDistance, LayerMask.NameToLayer("Terrain"));
+        Debug.DrawRay(gameObject.transform.position, Vector3.down * rayCastDistance,Color.red);
+        if (hasHit) {
+            canJump = true;
+        }
+
     }
 }
